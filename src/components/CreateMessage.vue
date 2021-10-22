c6d41f0bd82a492527a00db80483535c06765d0c
Andrew T. Lim <lim243@purdue.edu>
2021-10-22 19:06:49 -0400

    Add emoji package

--- src/components/CreateMessage.vue
+++ src/components/CreateMessage.vue
@@ -2,12 +2,23 @@
   <div class="create-message flex-grow-0 py-3 px-4 border-top">
     <form class="inline" @submit.prevent="onSendMessage">
       <div class="input-group">
+        <twemoji-picker
+          :emojiData="emojiDataAll"
+          :emojiGroups="emojiGroups"
+          :skinsSelection="false"
+          :searchEmojisFeat="true"
+          searchEmojiPlaceholder="Search here."
+          searchEmojiNotFound="Emojis not found."
+          isLoadingLabel="Loading..."
+          @emojiUnicodeAdded="onSelectEmoji"
+        />
         <input
           v-model="message"
           type="text"
           class="form-control"
           placeholder="Type your message"
         />
+
         <button class="btn btn-primary" type="submit">Send</button>
       </div>
     </form>
@@ -18,14 +29,25 @@
 import { mapState } from "vuex";
 import { collection, addDoc, updateDoc, doc } from "firebase/firestore";
 import { db } from "@/firebase/db";
+import { TwemojiPicker } from "@kevinfaguiar/vue-twemoji-picker";
+import EmojiAllData from "@kevinfaguiar/vue-twemoji-picker/emoji-data/en/emoji-all-groups.json";
+import EmojiGroups from "@kevinfaguiar/vue-twemoji-picker/emoji-data/emoji-groups.json";
 
 export default {
   data() {
     return {
-      message: null,
+      message: "",
+      showEmoji: false,
     };
   },
+  components: {
+    "twemoji-picker": TwemojiPicker,
+  },
   methods: {
+    onSelectEmoji(emoji) {
+      console.log(emoji);
+      this.message += emoji;
+    },
     onSendMessage() {
       if (this.message) {
         // Send message to store and update db
@@ -51,11 +73,17 @@ export default {
         // this.$store.dispatch("sendMessage", newMsg);
         console.log("sendMessage", newMsg);
       }
-      this.message = null;
+      this.message = "";
     },
   },
   computed: {
     ...mapState(["user", "currentRoom"]),
+    emojiDataAll() {
+      return EmojiAllData;
+    },
+    emojiGroups() {
+      return EmojiGroups;
+    },
   },
 };
 </script>
