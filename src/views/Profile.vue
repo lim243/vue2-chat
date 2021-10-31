9a619b85a2b9118dd680c3bfac1adcb347c985b9
Andrew T. Lim <lim243@purdue.edu>
2021-10-31 11:19:13 -0400

    Style profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,19 +1,47 @@
 <template>
-  <div class="profile">
-    <div class="card horizontal" style="max-width:400px;margin:0 auto;" v-if="user">
-      <div class="card-image" style="margin-top:25px;margin-left:10px;">
-        <img
-          :src="user.photoURL"
-          style="width:75px;height:75px;border-radius:50%;border:4px solid #333"
-        />
-      </div>
-      <div class="card-stacked">
-        <div class="card-content">
-          <p>
-            name: <strong>{{ user.displayName }}</strong
-            ><br />email:<strong>{{ user.email }}</strong
-            ><br />uid: <strong>{{ user.uid }}</strong> <br />provider:
-            <strong class="teal-text">{{ user.providerData[0].providerId }}</strong>
+  <div class="d-flex align-items-start justify-content-center">
+    <div class="card w-75">
+      <div class="bg-white w-100 rounded border-light p-4 p-lg-5 cover-photo"></div>
+      <div class="card-body bg-white d-flex align-items-start justify-content-center">
+        <div v-if="user.providerData[0].providerId === 'google.com'">
+          <img
+            :src="user.photoURL"
+            style="width:75px;height:75px;border-radius:50%;border:4px solid #FB5B0D"
+          />
+          <h4 class="h3">
+            {{ user.displayName }}
+          </h4>
+          <h5 class="font-weight-normal">{{ user.email }}</h5>
+          <p class="text-gray mb-4 btn bg-danger text-white">
+            GOOGLE
+          </p>
+        </div>
+
+        <div v-if="user.providerData[0].providerId === 'github.com'">
+          <img
+            :src="user.photoURL"
+            style="width:75px;height:75px;border-radius:50%;border:4px solid #333"
+          />
+          <h4 class="h3">
+            {{ user.displayName }}
+          </h4>
+          <h5 class="font-weight-normal">{{ user.email }}</h5>
+          <p class="text-gray mb-4 btn bg-dark text-white">
+            GITHUB
+          </p>
+        </div>
+
+        <div v-if="user.providerData[0].providerId === 'password'">
+          <img
+            :src="user.photoURL"
+            style="width:75px;height:75px;border-radius:50%;border:4px solid #96D1FF"
+          />
+          <h4 class="h3">
+            {{ user.displayName }}
+          </h4>
+          <h5 class="font-weight-normal">{{ user.email }}</h5>
+          <p class="text-gray mb-4 btn bg-secondary text-white">
+            PASSWORD
           </p>
         </div>
       </div>
@@ -31,3 +59,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.cover-photo {
+  background-image: url("../assets/leaves.jpg");
+  background-repeat: no-repeat !important;
+  background-position: top center !important;
+  min-height: 200px;
+}
+</style>
